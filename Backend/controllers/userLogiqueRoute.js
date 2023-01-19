8564645ec4ef1fee442f6d3b7ba106cd9205862a
Ascaris29 <solenelema2009@hotmail.com>
2023-01-19 17:52:47 +0100

    fixed untracked files

--- Backend/controllers/userLogiqueRoute.js
+++ Backend/controllers/userLogiqueRoute.js
@@ -1,21 +1,41 @@
 //importation modele de base de données user
 const User = require('../models/userModel')
 
+//importation de bcrypt pour hasher le password
+const bcrypt = require('bcrypt')
+
+//importation crypto-js pour chiffrer l'adresse email
+const cryptojs = require('crypto-js');
+
+//importation de variable d'environnement
+const dotenv = require('dotenv').config()
+
+
 //middleware signup
 // création d'une donnée user à partir du modele dans le fichier userModel
 // récupération des données user grace à req.body qui recupere le corps de la requete que l'utilisateur a tapé
 exports.signup = (req, res) => {
-    const user = new User({
-        email : req.body.email,
-        password : req.body.password
-    });
+    //chiffrer l'email avant de l'envoyer 
+    const emailCryptoJs = cryptojs.HmacSHA256(req.body.email, process.env.CRYPTOJS_EMAIL).toString();
+
+
+    // hasher le mot de passe avant de l'envoyer en bdd
+    // 10 = nombres de tours de l'algorithme
+    bcrypt.hash(req.body.password, 10)
+    .then((hash) => {
+        // ce qui va être envoyé dans la bdd avec le mot de passe hashé
+        const user = new User({
+            email : emailCryptoJs,
+            password : hash
+        });
     //enregistrer la donnée user dans la base de donnée
     user
     .save()
     .then(() => res.status(201).json({message : "Utilisateur crée et sauvegardé"}))
     .catch((error) => res.status(500).json({error}))
 
-
+    })
+    .catch((err) => res.status(500).json(err))
 }
 
 
--- Backend/models/userModel.js
+++ Backend/models/userModel.js
@@ -1,17 +1,20 @@
 //importation mongoose
 const mongoose = require('mongoose');
 
+//importation plugin email unique
+const uniqueValidator = require('mongoose-unique-validator');
+
 //création du modèle de donnée mongoose pour créer un utilisateur (sign up)
 // ne pas oublier la clé unique pour que l'email soit unique
 //ne pas oublier le hashage du mot de passe
 //si erreur 11000, supprimer index email dans indexes, car option unique : true
 const userSchema = mongoose.Schema({
     email : {type : String, required : true, unique : true}, 
-    // hasher le mot de passe avant de l'envoyer en bdd
     password : {type : String, required : true}
 })
 
-
+//application du plugin uniqueValidator
+userSchema.plugin(uniqueValidator);
 
 
 //exportation du module avec la méthode model
